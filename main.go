c0f5f1758703424f15a578d64830bdbc9e8dc3c9
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2023-01-23 13:54:43 +0900

    echo導入

--- main.go
+++ main.go
@@ -1,28 +1,32 @@
 package main
 
 import (
-	"log"
 	"net/http"
-	"qrchecker1/database"
+	// "qrchecker1/database"
+
+	"github.com/labstack/echo/v4"
 )
 
-func rootHander(w http.ResponseWriter, r *http.Request) {
-	db := database.Connect()
-	defer db.Close()
+// func rootHander(w http.ResponseWriter, r *http.Request) {
+// 	db := database.Connect()
+// 	defer db.Close()
 
-	err := db.Ping()
+// 	err := db.Ping()
 
-	if err != nil {
-		w.Write([]byte("データベース接続失敗"))
-		return
-	} else {
-		w.WriteHeader(200)
-		w.Header().Set("Content-Type", "text/html; charset=utf8")
-		w.Write([]byte("データベース接続成功"))
-	}
-}
+// 	if err != nil {
+// 		w.Write([]byte("データベース接続失敗"))
+// 		return
+// 	} else {
+// 		w.WriteHeader(200)
+// 		w.Header().Set("Content-Type", "text/html; charset=utf8")
+// 		w.Write([]byte("データベース接続成功"))
+// 	}
+// }
 
 func main() {
-	http.HandleFunc("/", rootHander)
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, world!")
+	})
+	e.Logger.Fatal(e.Start(":8080"))
 }
