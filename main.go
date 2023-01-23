987ee150f4ef1e74eb43deb493d343cdaf05bb94
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2023-01-23 17:19:43 +0900

    データの取得"DB→json"

--- main.go
+++ main.go
@@ -1,26 +1,43 @@
 package main
 
 import (
-	"net/http"
 	"qrchecker1/database"
+
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
-func hello(c echo.Context) error {
-	database.Connect()
-	sqlDB, _ := database.DB.DB()
-	defer sqlDB.Close()
-	err := sqlDB.Ping()
-	if err != nil {
-		return c.String(http.StatusInternalServerError, "データベース接続失敗")
-	} else {
-		return c.String(http.StatusOK, "Hello, World!")
+type User struct {
+	Id       int    `json:"id" param:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func getUsers(c echo.Context) error {
+	users := []User{}
+	database.DB.Find(&users)
+	return c.JSON(http.StatusOK, users)
+}
+
+func getUser(c echo.Context) error {
+	user := User{}
+	if err := c.Bind(&user); err != nil {
+		return err
 	}
+	database.DB.Take(&user)
+	return c.JSON(http.StatusOK, user)
 }
 
 func main() {
 	e := echo.New()
-	e.GET("/", hello)
+	database.Connect()
+	sqlDB, _ := database.DB.DB()
+	defer sqlDB.Close()
+
+	e.GET("/users", getUsers)
+	e.GET("/users/:id", getUser)
+
 	e.Logger.Fatal(e.Start(":8080"))
 }
