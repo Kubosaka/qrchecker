b52e127a154aea3e3db56b03efc92382d9bccc55
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2023-01-23 14:21:30 +0900

    変数名変更"mailadress"→"email"

--- initdb.d/init.sql
+++ initdb.d/init.sql
@@ -7,11 +7,11 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   id           INT(10) unsigned not null auto_increment,
   name     VARCHAR(255) not null,
-  mailadress varchar(255) not null,
+  email varchar(255) not null,
   passward varchar(255) not null,
   primary key (id)
 );
 
-INSERT INTO users (id, name, mailadress,passward) VALUES (1, "Yamada", "yamada@example.com","aaaaaa");
-INSERT INTO users (id, name, mailadress,passward) VALUES (2, "Kubosaka", "kubosaka@example.com","bbbbbb");
-INSERT INTO users (id, name, mailadress,passward) VALUES (3, "Kubosaka2", "kubosaka2@example.com","ccccccc");
+INSERT INTO users (id, name, email,passward) VALUES (1, "Yamada", "yamada@example.com","aaaaaa");
+INSERT INTO users (id, name, email,passward) VALUES (2, "Kubosaka", "kubosaka@example.com","bbbbbb");
+INSERT INTO users (id, name, email,passward) VALUES (3, "Kubosaka2", "kubosaka2@example.com","ccccccc");
