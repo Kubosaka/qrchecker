9964400eaadaea06dac8ec9a28e38535c7df11e8
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2023-01-28 18:23:38 +0900

    fix

--- mysql/db/init.sql
+++ mysql/db/init.sql
@@ -20,9 +20,9 @@ CREATE TABLE users (
 
 
 
-INSERT INTO users (id, user_id, faculty, department, major, course, grade, name, email, password) VALUES (1, 10000, "Business_Administration", "Business_Administration", "Management_Information Systems", "Bachelor", 3, "IyodaAkira", "iyoda@example.com", "aaaaaaa");
-INSERT INTO users (id, user_id, faculty, department, major, course, grade, name, email, password) VALUES (2, 20000, "Business_Administration", "Business_Administration", "Management_Information Systems", "Bachelor", 2, "YamadaTarou", "yamada@example.com", "bbbbbbb");
-INSERT INTO users (id, user_id, faculty, department, major, course, grade, name, email, password) VALUES (3, 30000, "Business_Administration", "Business_Administration", "Management_Information Systems", "Bachelor", 1, "SuzukiHanako", "suzuki@example.com", "bbbbbbb");
-INSERT INTO users (id, user_id, faculty, department, major, course, grade, name, email, password) VALUES (4, 40000, "Business_Administration", "Business_Administration", "Management_Information Systems", "Bachelor", 2, "SatouRin", "satou@example.com", "bbbbbbb");
-INSERT INTO users (id, user_id, faculty, department, major, course, grade, name, email, password) VALUES (5, 50000, "Business_Administration", "Business_Administration", "Management_Information Systems", "Bachelor", 3, "HakaseTarou", "hakase@example.com", "ccccccc");
+INSERT INTO users (id, user_id, faculty, department, major, course, grade, name, email, password) VALUES (1, 10000, "Business_Administration", "Business_Administration", "Management_Information_Systems", "Bachelor", 3, "IyodaAkira", "iyoda@example.com", "aaaaaaa");
+INSERT INTO users (id, user_id, faculty, department, major, course, grade, name, email, password) VALUES (2, 20000, "Business_Administration", "Business_Administration", "Management_Information_Systems", "Bachelor", 2, "YamadaTarou", "yamada@example.com", "bbbbbbb");
+INSERT INTO users (id, user_id, faculty, department, major, course, grade, name, email, password) VALUES (3, 30000, "Business_Administration", "Business_Administration", "Management_Information_Systems", "Bachelor", 1, "SuzukiHanako", "suzuki@example.com", "bbbbbbb");
+INSERT INTO users (id, user_id, faculty, department, major, course, grade, name, email, password) VALUES (4, 40000, "Business_Administration", "Business_Administration", "Management_Information_Systems", "Bachelor", 2, "SatouRin", "satou@example.com", "bbbbbbb");
+INSERT INTO users (id, user_id, faculty, department, major, course, grade, name, email, password) VALUES (5, 50000, "Business_Administration", "Business_Administration", "Management_Information_Systems", "Bachelor", 3, "HakaseTarou", "hakase@example.com", "ccccccc");
 
